4eda8efb31ddfead2bcc8796cf1d30ff63bd43d0
Iip Shoifuddin <115620789+shoifuddiniip@users.noreply.github.com>
2024-10-25 21:25:31 +0700

    change colum name

--- app/Models/LeadDistributionTracker.php
+++ app/Models/LeadDistributionTracker.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class LeadDistributionTracker extends Model
 {
-    protected $fillable = ['last_assigned_salesperson_id'];
+    protected $fillable = ['last_sales_id'];
 }
